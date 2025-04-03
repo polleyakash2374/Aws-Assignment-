2dcf805fdb1aa44090ba9006c409a461141e8f0a
polleyakash2374 <polleyakash1810@gmail.com>
2025-04-03 11:10:22 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
+  res.send('Welcome to MCKV')
 })
 
 app.get('/courses', function (req, res) {
